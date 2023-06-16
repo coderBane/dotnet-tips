463949abd3b16ae1e1aebf8a718bf6e62424b6ab
coderbane <110666273+coderBane@users.noreply.github.com>
2023-06-16 19:01:37 +0100

    add - sqlserver and postgres migrations

---
+++ efcore/MultipleDbProviders/Migrators/Migrators.Mssql/Migrations/20230616164434_TestMig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.Mssql.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Lastname = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_Firstname_Lastname",
+                table: "People",
+                columns: new[] { "Firstname", "Lastname" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ efcore/MultipleDbProviders/Migrators/Migrators.Npgsql/Migrations/20230616164643_TestMig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Migrators.Npgsql.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Firstname = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Lastname = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_Firstname_Lastname",
+                table: "People",
+                columns: new[] { "Firstname", "Lastname" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
