e882a9d8083a8362b0eda459aed57b6703d5aa5b
coderbane <110666273+coderBane@users.noreply.github.com>
2023-06-16 02:39:41 +0100

    add - api project (one database provider)

---
+++ efcore/MultipleDbProviders/Data/Migrations/20230616010739_TestMig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Firstname = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    Lastname = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_Firstname_Lastname",
+                table: "People",
+                columns: new[] { "Firstname", "Lastname" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ efcore/MultipleDbProviders/Data/SampleDbContext.cs
@@ -0,0 +1,35 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class SampleDbContext : DbContext
+{
+	public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Person> People => Set<Person>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Person>(b =>
+        {
+            b.Property(p => p.Firstname).HasMaxLength(64);
+            b.Property(p => p.Lastname).HasMaxLength(64);
+
+            b.HasIndex(p => new { p.Firstname, p.Lastname });
+        });
+    }
+}
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public required string Firstname { get; set; }
+
+    public required string Lastname { get; set; }
+}
+
